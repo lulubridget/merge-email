acef4b1bf61959a89da00c1a4a37d0dd44377643
Lu Wang <99560066+lulubridget@users.noreply.github.com>
2023-12-27 13:46:52 -0800

    initial code

---
+++ main.py
@@ -0,0 +1,15 @@
+placeholder = "[name]"
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+    print(names)
+with open("./Input/Letters/starting_letter.txt") as letters_file:
+    letter = letters_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter.replace(placeholder, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+           completed_letter.write(new_letter)
+
+
+
+
